e96fe1172ad186641dc52f36f9a14e52e4d13645
ordta11 <ordta11@29-6>
2025-10-21 10:20:18 +0300

    17263

--- main.js
+++ main.js
@@ -10,14 +10,56 @@ function func3()
 {
     input.value+="3";
 }
-
+function func4()
+{
+    input.value+="4";
+}
+function func5()
+{
+    input.value+="5";
+}
+function func6()
+{
+    input.value+="6";
+}
+function func7()
+{
+    input.value+="7";
+}
+function func8()
+{
+    input.value+="8";
+}
+function func9()
+{
+    input.value+="9";
+}
+function func0()
+{
+    input.value+="0";
+}
 
 let input = document.getElementById("input")
 let but1 = document.getElementById("b1")
 let but2 = document.getElementById("b2")
 let but3 = document.getElementById("b3")
+let but4 = document.getElementById("b4")
+let but5 = document.getElementById("b5")
+let but6 = document.getElementById("b6")
+let but7 = document.getElementById("b7")
+let but8 = document.getElementById("b8")
+let but9 = document.getElementById("b9")
+let but0 = document.getElementById("b0")
+
 
 but1.addEventListener("click", func1);
 but2.addEventListener("click", func2);
-but2.addEventListener("click", func3);
+but3.addEventListener("click", func3);
+but4.addEventListener("click", func4);
+but5.addEventListener("click", func5);
+but6.addEventListener("click", func6);
+but7.addEventListener("click", func7);
+but8.addEventListener("click", func8);
+but9.addEventListener("click", func9);
+but0.addEventListener("click", func0);
 
